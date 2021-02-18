64cceed27a9734a5b055244671f1db7262fdf836
liz-allison <eeallison@gmail.com>
2021-02-17 18:20:17 -0800

    Updates as of 2/17

--- server.js
+++ server.js
@@ -2,18 +2,41 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
-/* Middleware*/
+//Dependencies
+const bodyParser = require('body-parser')
+//Middleware
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
-// Initialize the main project folder
-app.use(express.static('website'));
+// Initialize the main project folder(folder names is "website")
+app.use(express.static('website'));  
 
+//GET route to return endpoint data
+app.get('/', (req, res) => {
+    res.send(projectData);
+});
+
+// POST route to store data
+const data = [];
+app.post('/', addZipAndFeelings);
+
+function addZipAndFeelings (req,res){
+    data.push(req.body);
+};
 
 // Setup Server
+const port = 8000
+
+//Callback
+const server = app.listen(port, ()=> {
+    console.log(`running on localhost: ${port}`)});  
--- website/app.js
+++ website/app.js
@@ -1,5 +1,52 @@
 /* Global Variables */
+//Use of API to access weather database
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip='
+const apiKey = '3c695c0303609cf395d095d701f8ad45'
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+//GET request to weather info API
+document.getElementById('generate').addEventListener('click', performAction);
+
+function performAction(e) {
+    const newZip = document.getElementById('zip').value;
+    const feelings = document.getElementById('feelings').value;
+    getWeather(baseURL, newZip, apiKey)
+}
+
+const getWeather = async(baseURL, newZip, apiKey) => {
+    const res = await fetch(baseURL + newZip + apiKey)
+    try {
+        const data = await res.json();
+        console.log(data)
+        return data;
+    } catch (error) {
+        console.log("error", error);
+    }
+}
+
+//Async POST
+const postData = async ( url = '', data = {})=>{
+
+    const response = await fetch(url, {
+    method: 'POST', 
+    credentials: 'same-origin', 
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header        
+  });
+
+    try {
+      const newData = await response.json();
+      return newData;
+    }catch(error) {
+    console.log("error", error);
+    }
+};
+
+//Might use this variable later
+//const request = await fetch(`${baseURL}?zip=${zip.value},&appid=${apiKey}&units=metric`);
+
